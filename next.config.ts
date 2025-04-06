10d91cb6d9a2e66fd22fc7f24406e2e0b0cdfb67
Seu Nome <seu.email@exemplo.com>
2025-04-05 21:57:16 -0300

    fix: adiciona tipagem para next-pwa e ajusta configuração

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 import withPWA from 'next-pwa';
 
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+};
+
 const config = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development'
-})({
-  reactStrictMode: true,
-});
+})(nextConfig);
 
 export default config;
